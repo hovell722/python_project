dc90203c59cd5e18f028e8982c8c61b1fa9a5698
James Hovell <j.hovell722@gmail.com>
2020-04-09 13:26:29 +0100

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# python_project
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: python_project
+# Recipe:: default
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
